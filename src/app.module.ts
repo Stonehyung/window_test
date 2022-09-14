581172e4813e99a4ab3930e1309afdf41fb6499a
Stone hyung <qkdgkr1256@gmail.com>
2022-09-14 12:44:35 +0900

    typeorm 연결완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,8 +13,65 @@ import { ChannelsModule } from './channels/channels.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { UsersService } from './users/users.service';
 import { WorkspacesService } from './workspaces/workspaces.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormconfig from '../ormconfig'
+import { ChannelChats } from './entities/ChannelChats';
+import { ChannelMembers } from './entities/ChannelMembers';
+import { Channels } from './entities/Channels';
+import { DMs } from './entities/DMs';
+import { Mentions } from './entities/Mentions';
+import { Users } from './entities/Users';
+import { WorkspaceMembers } from './entities/WorkspaceMembers';
+import { Workspaces } from './entities/Workspaces';
+const getdata = async () => {
+
+  return{
+    DB_USERNAME: 'root',
+    DB_PASSWORD: 'root',
+    DB_DATABASE: 'sleact'
+  }
+}
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, DmsModule,  ChannelsModule, WorkspacesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true , load: [getdata]}),
+    UsersModule,
+    WorkspacesModule,
+    ChannelsModule,
+    DmsModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async(configService : ConfigService) => {
+      return {
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [
+          ChannelChats,
+          ChannelMembers,
+          Channels,
+          DMs,
+          Mentions,
+          Users,
+          WorkspaceMembers,
+          Workspaces,
+        ],
+        migrations: [__dirname + '/src/migrations/*.ts'],
+        cli: { migrationsDir: 'src/migrations' },
+        autoLoadEntities: true,
+        charset: 'utf8mb4',
+        synchronize: false,
+        logging: true,
+        keepConnectionAlive: true,
+
+
+      } 
+      }
+    }),
+
+  ],
   controllers: [AppController, ChannelsController],
   providers: [AppService, ConfigService, ChannelsService, UsersService, WorkspacesService],
 })
